614db3ceac688091d57d5d51c87bf402cb22a3b0
Tatiana Kosenko <osmile378@gmail.com>
2023-10-08 11:03:37 +0300

    Seminar 6 and homework 6 added

---
+++ Homework6/Program.cs
@@ -0,0 +1,78 @@
+﻿
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество цифр: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        int temp = Convert.ToInt32(Console.ReadLine());
+        array[i] = temp;
+    }
+    return array;
+}
+
+int FindNumGreaterNull(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string GetArrayStr(int[] array)
+{
+    string arrayStr = string.Empty;
+    arrayStr += "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayStr += $"{array[i]}";
+        arrayStr += i < array.Length - 1 ? ", " : "";
+    }
+    arrayStr += "]";
+    return arrayStr;
+}
+
+int[] array = GetArray(n);
+
+Console.WriteLine($"В массиве: {GetArrayStr(array)}, {FindNumGreaterNull(array)} - положительных чисел");
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+string FindNumbers(double b1, double k1, double b2, double k2)
+{
+    string result = string.Empty;
+
+    double x = 0;
+    double y = 0;
+
+    x = (b1 - b2) / (k2 - k1);
+    y = k1 * x + b1;
+    result = $"({x}; {y})";
+    return result; 
+}
+
+Console.WriteLine(FindNumbers(b1, k1, b2, k2));
---
+++ Seminar6/Program.cs
@@ -0,0 +1,140 @@
+﻿/*
+// Задача 39: Напишите программу, которая перевернёт одномерный
+// массив (последний элемент будет на первом месте, а первый - на
+// последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение массива: ");
+int minArr = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение массива: ");
+int maxArr = Convert.ToInt32(Console.ReadLine());
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t");
+    }
+    Console.Write("\b\b\b\b]\n\n");
+}
+
+int[] GenArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);       
+    }
+    return array;
+}
+
+int[] ReverseArray(int[] array)
+{
+    int temp = 0;
+    int lastNum = array.Length - 1;
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        temp = array[i];
+        array[i] = array[lastNum - i];
+        array[lastNum - i] = temp;
+    }
+    return array;
+}
+
+int[] array = GenArray(n, minArr, maxArr);
+PrintArray(array);
+array = ReverseArray(array);
+PrintArray(array);
+
+
+// Задача 40: Напишите программу, которая принимает на вход три числа и
+// проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+bool CheckTriangle(int a, int b, int c)
+{
+    if((a < b + c) && (b < a + c) && (c < a + b))
+    {
+        return true;
+    }
+    return false;
+}
+
+Console.Write("Введите A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Имеется ли треугольник: {CheckTriangle(a, b, c)}");
+
+
+// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10 
+
+string ChangeTenToTwo(int numTen)
+{
+    string newNum = string.Empty;
+    while (numTen > 1)
+    {
+        newNum = $"{numTen % 2}" + newNum;
+        numTen /= 2;
+        newNum = (numTen == 1 ? "1" : "") + newNum;
+    }
+
+    return newNum;
+}
+
+Console.Write("Введите число в десятичной системе счисления: ");
+int numTen = Convert.ToInt32(Console.ReadLine());
+string numTwo = ChangeTenToTwo(numTen);
+Console.WriteLine($"Если перевести число {numTen} в двоичную СС, то получится: {numTwo}");
+*/
+// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+// Первые два числа: 0, 1
+// Если N = 5: 0 1 1 2 3
+
+void PrintArrayNew(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t");
+    }
+    Console.Write("\b\b\b\b]\n\n");
+}
+
+Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+array[0] = 0;
+array[1] = 1;
+for (int i = 2; i < num; i++)
+{
+    array[i] = array[i - 1] + array[i - 2];
+}
+
+PrintArrayNew(array);
+
+// Задача 45: Напишит программу, которая будет создавать копию
+// заданного массива с помощью поэлементного копирования
+int[] CopyArrayTo(int[] array)
+{
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+
+int[] arr = new int[] {1, 2, 3, 4, 5, 6};
+int[] newArr = CopyArrayTo(arr);
+PrintArrayNew(newArr);
